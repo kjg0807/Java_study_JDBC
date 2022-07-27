72b5464249825338d35a3572fae75402392f8162
kjg0807 <kjg08070@naver.com>
2022-07-27 12:20:03 +0900

    DBTest end

--- src/com/naver/test/TestMain.java
+++ src/com/naver/test/TestMain.java
@@ -1,9 +1,21 @@
 package com.naver.test;
 
+import com.naver.util.DBConnetor_login;
+
 public class TestMain
 {
 	public static void main(String[] args)
 	{
-		
+		DBConnetor_login db = new DBConnetor_login();
+
+		try
+		{
+			db.getConnection();
+		}
+		catch (Exception e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
